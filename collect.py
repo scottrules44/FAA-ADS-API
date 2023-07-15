bbcec104ae155f729e9db72dac916031414c4fbc
Scott Harrison <scottrules44@gmail.com>
2023-07-14 20:39:11 -0500

    Fixes

--- collect.py
+++ collect.py
@@ -7,8 +7,8 @@ print("test123")
 def get_pretty_print(json_object):
     return json.dumps(json_object, sort_keys=True, indent=4, separators=(',', ': '))
 
-request = requests.get(drsUrl, headers = {"x-api-key": sys.argv[0]})
-print(sys.argv)
+request = requests.get(drsUrl, headers = {"x-api-key": sys.argv[1]})
+print(request)
 print("-------")
 print(request.text)
 f = open("data.json", "a")
