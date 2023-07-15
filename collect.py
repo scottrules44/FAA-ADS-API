6d0dc245c85affc958fbcfb4a596a2859e2a7afd
Scott Harrison <scottrules44@gmail.com>
2023-07-14 20:35:28 -0500

    Fixes

--- collect.py
+++ collect.py
@@ -3,11 +3,12 @@ import json
 import sys
 drsUrl = "https://drs.faa.gov/api/drs/data-pull/ADFRAWD"
 
+print("test123")
 def get_pretty_print(json_object):
     return json.dumps(json_object, sort_keys=True, indent=4, separators=(',', ': '))
 
 request = requests.get(drsUrl, headers = {"x-api-key": sys.argv[0]})
-
+print(request)
 f = open("data.json", "a")
 f.write(get_pretty_print(request.text))
 f.close()
