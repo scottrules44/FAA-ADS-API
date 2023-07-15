f15c097c1fe54b5949a13faaa55dc577cfb68de2
Scott Harrison <scottrules44@gmail.com>
2023-07-14 21:35:15 -0500

    Clean Up

--- collect.py
+++ collect.py
@@ -3,13 +3,26 @@ import json
 import sys
 drsUrl = "https://drs.faa.gov/api/drs/data-pull/ADFRAWD"
 
-print("test123")
+def delete_and_modify_first_line(file_path, new_character):
+    # Read the input file
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    # Remove the first line
+    lines = lines[1:]
+
+    # Modify the new first line
+    if lines:
+        lines[0] = new_character + lines[0]
+
+    # Write the modified content to a new file
+    with open(file_path, 'w') as file:
+        file.writelines(lines)
 
 request = requests.get(drsUrl, headers = {"x-api-key": sys.argv[1]})
-print(request)
-print("-------")
-print(request.text)
 obj = json.loads(request.text)
 f = open("data.json", "a")
 f.write(json.dumps(obj, indent=4))
 f.close()
+
+delete_and_modify_first_line("data.json", "{")
