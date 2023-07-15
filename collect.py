821a907899d1486a791974af3f73c8afe6ba353a
Scott Harrison <scottrules44@gmail.com>
2023-07-14 20:47:54 -0500

    Test 2

--- collect.py
+++ collect.py
@@ -4,13 +4,12 @@ import sys
 drsUrl = "https://drs.faa.gov/api/drs/data-pull/ADFRAWD"
 
 print("test123")
-def get_pretty_print(json_object):
-    return json.dumps(json_object, sort_keys=True, indent=4, separators=(',', ': '))
 
 request = requests.get(drsUrl, headers = {"x-api-key": sys.argv[1]})
 print(request)
 print("-------")
 print(request.text)
+obj = json.loads(request.text)
 f = open("data.json", "a")
-f.write(get_pretty_print(request.text))
+f.write(json.dumps(obj, indent=4))
 f.close()
